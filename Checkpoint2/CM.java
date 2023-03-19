d9277c297594286840f7de49ae062f6ef8189300
Farid Hamid <farid@Seijuro.local>
2023-03-19 15:20:05 -0400

    The output now gets redirected to their specified files when flags are present

--- Checkpoint2/CM.java
+++ Checkpoint2/CM.java
@@ -3,26 +3,31 @@ import absyn.*;
    
 class CM {
   static public void main(String argv[]) {    
+
     boolean a_flag = false;
     boolean s_flag = false;
     
     for (String s: argv)
-      if(s.equals("-a"))
-        a_flag = true;
-      else if (s.equals("-s"))
-        s_flag = true;
-
+    if(s.equals("-a"))
+    a_flag = true;
+    else if (s.equals("-s"))
+    s_flag = true;
+    
     /* Start the parser */
     try {
       parser p = new parser(new Lexer(new FileReader(argv[0])));
       Absyn result = (Absyn)(p.parse().value);      
-
+      
       if (a_flag && result != null) {
+        PrintStream out = new PrintStream(new FileOutputStream("AbstractSyntaxTree.txt"));
+        System.setOut(out);
         System.out.println("The abstract syntax tree is:");
         ShowTreeVisitor visitor = new ShowTreeVisitor();
         result.accept(visitor, 0); 
       }
       if (s_flag && result != null) {
+        PrintStream out = new PrintStream(new FileOutputStream("SymbolTable.txt"));
+        System.setOut(out);
         System.out.println("Entering the global scope:");
         SemanticAnalyzer visitor = new SemanticAnalyzer();
         result.accept(visitor, 0);
