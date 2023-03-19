f58b8b046a98863c380454549651ead177e54979
Farid Hamid <farid@Seijuro.local>
2023-03-19 16:42:37 -0400

    Added a toString() method to return the name of the variable for VarExp

--- Checkpoint2/absyn/SimpleVar.java
+++ Checkpoint2/absyn/SimpleVar.java
@@ -9,6 +9,10 @@ public class SimpleVar extends Var {
         this.name = name;
     }
 
+    public String toString() {
+        return name;
+    }
+
     public void accept( AbsynVisitor visitor, int level ) {
         visitor.visit( this, level );
     }
